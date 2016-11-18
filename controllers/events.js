8b4550946ab73b02d494a8efc0b1db4360dd76eb
rajbrar625 <rajbrar625@hotmail.com>
2016-11-18 05:11:20 +0000

    added-response-redirect

--- controllers/events.js
+++ controllers/events.js
@@ -95,6 +95,7 @@ function saveEvent(request, response){
   
   if (contextData.errors.length === 0) {
     var newEvent = {
+      id: events.all.length,
       title: request.body.title,
       location: request.body.location,
       image: request.body.image,
@@ -102,7 +103,7 @@ function saveEvent(request, response){
       attending: []
     };
     events.all.push(newEvent);
-    response.redirect('/events/' + events.all.length);
+    response.redirect('/events/' +   newEvent.id);
   }else{
     response.render('create-event.html', contextData);
   }
